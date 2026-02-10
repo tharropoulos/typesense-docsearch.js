cbdd98b08d63f0f21ad3b2c7974c9664e5de3c79
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-02-10 18:13:54 +0200

    wip-types-searchclient

--- packages/docsearch-react/src/useSearchClient.ts
+++ packages/docsearch-react/src/useSearchClient.ts
@@ -1,29 +1,95 @@
-import { liteClient } from 'algoliasearch/lite';
+import type { SearchResponses } from 'algoliasearch/lite';
 import React from 'react';
+import { SearchClient as TypesenseSearchClient } from 'typesense';
+import type { ConfigurationOptions as TypesenseConfigurationOptions } from 'typesense/lib/Typesense/Configuration';
+import type { SearchResponse as TypesenseSearchResponse, DocumentSchema } from 'typesense/lib/Typesense/Documents';
+import type { MultiSearchRequestSchema } from 'typesense/lib/Typesense/Types';
+import { SearchResponseAdapter as TypesenseSearchResponseAdapter } from 'typesense-instantsearch-adapter/lib/SearchResponseAdapter';
 
-import type { DocSearchTransformClient } from './DocSearch';
-import { version } from './version';
+import type { TypesenseDocsearchTransformClient } from './DocSearch';
+
+type AdaptedSearchResponse<T extends DocumentSchema> = {
+  hits: Array<AdaptedHit<T>>;
+  nbHits: number;
+  page: number;
+  nbPages: number;
+  hitsPerPage: number;
+  facets: Record<string, Record<string, number>>;
+  facets_stats: Record<string, Record<string, number>>;
+  query: string;
+  processingTimeMS: number;
+  renderingContent?: Record<string, unknown>;
+  appliedRules?: Array<Record<string, unknown>>;
+  userData?: unknown[];
+  parsed_nl_query?: TypesenseSearchResponse<T>['parsed_nl_query'];
+};
+
+type AdaptedHit<T extends DocumentSchema> = T & {
+  objectID: string;
+  // eslint-disable-next-line no-warning-comments
+  // TODO: find the exact types for these (this is written in js and I'm just winging it)
+  _snippetResult?: Record<string, any>;
+  _highlightResult?: Record<string, any>;
+  _rawTypesenseHit?: unknown;
+  _rawTypesenseConversation?: unknown;
+  _geoloc?: { lat: number; lng: number };
+  group_key?: string;
+  _group_key?: string;
+  _group_found?: number;
+  _grouped_hits?: Array<AdaptedHit<T>>;
+};
 
 export function useSearchClient(
-  appId: string,
-  apiKey: string,
-  transformSearchClient: (searchClient: DocSearchTransformClient) => DocSearchTransformClient,
-): DocSearchTransformClient {
+  transformSearchClient: (searchClient: TypesenseDocsearchTransformClient) => TypesenseDocsearchTransformClient,
+  typesenseServerConfig: TypesenseConfigurationOptions,
+): TypesenseDocsearchTransformClient {
   const searchClient = React.useMemo(() => {
-    const client = liteClient(appId, apiKey);
-    client.addAlgoliaAgent('docsearch', version);
+    const typesense = new TypesenseSearchClient(typesenseServerConfig);
 
-    // Since DocSearch.js relies on DocSearch React with an alias to Preact,
-    // we cannot add the `docsearch-react` user agent by default, otherwise
-    // it would also be sent on a DocSearch.js integration.
-    // We therefore only add the `docsearch-react` user agent if `docsearch.js`
-    // is not present.
-    if (/docsearch.js \(.*\)/.test(client.transporter.algoliaAgent.value) === false) {
-      client.addAlgoliaAgent('docsearch-react', version);
-    }
+    const client: TypesenseDocsearchTransformClient = {
+      search: async <T extends DocumentSchema>({
+        requests,
+      }: {
+        requests: Array<MultiSearchRequestSchema<T, string>>;
+      }): Promise<SearchResponses<T>> => {
+        const [request] = requests;
+        if (!request) {
+          return { results: [] };
+        }
+
+        const response = await typesense.multiSearch.perform<[T]>({
+          searches: [request],
+        });
+        const typesenseSearchResponseAdapter = new TypesenseSearchResponseAdapter(
+          response.results[0],
+          {
+            params: {
+              ...request,
+              highlightPreTag: '<mark>',
+              highlightPostTag: '</mark>',
+            },
+          },
+          {
+            geoLocationField: '',
+          },
+        );
+
+        const adapted: AdaptedSearchResponse<T> = typesenseSearchResponseAdapter.adapt();
+
+        return {
+          results: [
+            {
+              ...adapted,
+              index: request.collection,
+              params: '',
+            },
+          ],
+        };
+      },
+    };
 
     return transformSearchClient(client);
-  }, [appId, apiKey, transformSearchClient]);
+  }, [transformSearchClient, typesenseServerConfig]);
 
   return searchClient;
 }
