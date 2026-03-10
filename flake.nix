dbaa34fd11ad202bd22ae07420c0d6ff77c83f47
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-03-10 17:54:19 +0200

    chore(dev): add Nix dev shell

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "DocSearch dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.nodejs_22
+              pkgs.yarn-berry
+              pkgs.nodePackages.typescript
+            ];
+          };
+        });
+    };
+}
