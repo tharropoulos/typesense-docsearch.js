4bc7caf3dd8f6a69deb20db81a7f74a8e37f53f1
Fanis Tharropoulos <ftharropoulos@gmail.com>
2026-03-20 14:42:55 +0200

    chore: remove website package

--- packages/website/plugins/tailwind-loader.mjs
+++
@@ -1,9 +0,0 @@
-export default function () {
-  return {
-    name: 'tailwind-plugin',
-    configurePostCss(postcssOptions) {
-      postcssOptions.plugins = [require('@tailwindcss/postcss')];
-      return postcssOptions;
-    },
-  };
-}
